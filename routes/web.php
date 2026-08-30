ec0dbed781fe004cb14dd27549833988d322f551
Julián Cabrera <juliancabrersf@gmail.com>
2026-08-30 17:34:56 -0300

    simplifacion a ver si anda

--- routes/web.php
+++ routes/web.php
@@ -63,7 +63,10 @@ Route::middleware(['auth'])->group(function () {
 
     // Vistas principales y buscadores
     Route::get('/flights', [FlightController::class, 'index'])->name('flights.index');
-    Route::get('paths', [PathController::class, 'index'])->name('paths.index');
+    // Route::get('paths', [PathController::class, 'index'])->name('paths.index');
+    Route::get('/paths', function () {
+    return view('paths.index');
+})->name('paths.index');
     Route::post('paths', [PathController::class, 'show'])->name('paths.show');
 
     /*
